6c92f48d59bc228500ffb061ec1a40efad8b7f0e
martin <s195469@student.dtu.dk>
2020-11-17 14:05:33 +0100

    price calculated

--- netto.go
+++ netto.go
@@ -78,7 +78,7 @@ func main() {
 		}
 	}(shop.customers, closed)
 
-	go shoppingScenario(shop.tills, shop.customers)
+	go shopping(shop.tills, shop.customers)
 
 	go tillArea(&shop.done, shop.tills, closed)
 	shop.done.Wait()
@@ -99,41 +99,73 @@ func customerSpawner() (customer Customer) {
 	return customer
 }
 
-func shoppingScenario(tills []*Till, customersInShop chan Customer) {
+func shopping(tills []*Till, customersInShop chan Customer) {
+	var customer Customer
+
 	for {
 		select {
-		case tills[0].line <- <-customersInShop:
-		case tills[1].line <- <-customersInShop:
-		case tills[2].line <- <-customersInShop:
-		case tills[3].line <- <-customersInShop:
-		case tills[4].line <- <-customersInShop:
-		case tills[5].line <- <-customersInShop:
+		case customer = <-customersInShop:
+			time.Sleep(time.Duration(customer.shoppingDuration) * time.Millisecond)
+		case <-time.After(time.Second):
+			//No more customers in shop
+			return
+		}
+
+		select {
+		case tills[0].line <- customer:
+		case tills[1].line <- customer:
+		case tills[2].line <- customer:
+		case tills[3].line <- customer:
+		case tills[4].line <- customer:
+		case tills[5].line <- customer:
 		case <-time.After(3 * time.Second):
+			//All tills are blocked or closed
 			return
 		}
 	}
 }
 
+func (self Customer) getTotal() int {
+	var total int
+	for _, grocery := range self.groceries {
+		total += grocery.price
+	}
+	return total
+}
+
 func tillArea(wg *sync.WaitGroup, tills []*Till, closed chan bool) {
+	var customer Customer
 	defer wg.Done()
 	for {
 		select {
-		case customer := <-tills[0].line:
-			fmt.Println("Customer payed", customer)
-		case customer := <-tills[1].line:
-			fmt.Println("Customer payed", customer)
-		case customer := <-tills[2].line:
-			fmt.Println("Customer payed", customer)
-		case customer := <-tills[3].line:
-			fmt.Println("Customer payed", customer)
-		case customer := <-tills[4].line:
-			fmt.Println("Customer payed", customer)
-		case customer := <-tills[5].line:
-			fmt.Println("Customer payed", customer)
+		case customer = <-tills[0].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 1")
+			tills[0].cash += customer.getTotal()
+		case customer = <-tills[1].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 2")
+			tills[1].cash += customer.getTotal()
+		case customer = <-tills[2].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 3")
+			tills[2].cash += customer.getTotal()
+		case customer = <-tills[3].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 4")
+			tills[3].cash += customer.getTotal()
+		case customer = <-tills[4].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 5")
+			tills[4].cash += customer.getTotal()
+		case customer = <-tills[5].line:
+			fmt.Println("Customer payed", customer.getTotal(), "DKK , at till 6")
+			tills[5].cash += customer.getTotal()
 		case <-time.After(3 * time.Second):
 			select {
 			case <-closed:
 				fmt.Println("Shop is closed 4 REALZ")
+				fmt.Println("Till 1 made", tills[0].cash)
+				fmt.Println("Till 2 made", tills[1].cash)
+				fmt.Println("Till 3 made", tills[2].cash)
+				fmt.Println("Till 4 made", tills[3].cash)
+				fmt.Println("Till 5 made", tills[4].cash)
+				fmt.Println("Till 6 made", tills[5].cash)
 				return
 			case <-time.After(time.Second):
 				fmt.Println("Customers are slow... till is bored...")
